c30828c00aab6aa7245c72068a26942ff5e10ca8
Liyam <liyamdu93@gmail.com>
2023-02-22 20:20:49 +0100

    test3

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,6 +3,9 @@ import author from './../me.jpg'
 import fallingsnake from './../fallingsnake.jpg'
 import projetlaravel from './../projetlaravel.jpg'
 import damechinoise from './../damechinoise.jpg'
+import { Link } from 'react-router-dom';
+
+
 
 const Portfolio = () => {
   return (
@@ -21,8 +24,7 @@ const Portfolio = () => {
                 <img src={fallingsnake} class="img"/>
                 <h3 >Falling Snake</h3>
                 <p>Java</p>
-        <a href="/fallingsnake">Read More</a>
-      </div>
+<Link to="/fallingsnake">Read me </Link>      </div>
             </div>
   </div>
 
@@ -34,7 +36,7 @@ const Portfolio = () => {
         <img src={projetlaravel} class="img1"/>
                 <h3>Cours en Ligne®</h3>
                 <p>Laravel</p>
-        <a href="/coursenligne">Read More</a>
+        <Link to="/coursenligne">Read me </Link>
       </div>
     </div>
   </div>
@@ -45,7 +47,7 @@ const Portfolio = () => {
         <img src={damechinoise} class="img1" />
             <h3>Dame Chinoise</h3>
           <p>Ocaml</p>
-        <a href="/damechinoise">Read More</a>
+        <Link to="/damechinoise">Read me </Link>
       </div>
     </div>
   </div>
